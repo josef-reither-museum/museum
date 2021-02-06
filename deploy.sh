9140b6ce1c6b08b89ad9ad8ce6f4be6f2d60a18e
Dominik Inführ <dominik.infuehr@gmail.com>
2021-02-06 14:50:16 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 DIR=../josef-reither-museum.github.io
+pushd $DIR
+rm -rf *
+popd
 $HOME/bin/hugo -D -d $DIR
 cd $DIR
+git checkout -- CNAME
 git add -A
 git commit -m "update website"
 git push
