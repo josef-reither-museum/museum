409ff7b057844f00c2d9cb0c93f052ba256bcb73
Dominik Inführ <dominik.infuehr@gmail.com>
2021-02-06 14:56:19 +0100

    copy CNAME file into this repo

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-DIR=../josef-reither-museum.github.io
+DIR=`pwd`/../josef-reither-museum.github.io
 pushd $DIR
 rm -rf *
 popd
 $HOME/bin/hugo -D -d $DIR
+cp CNAME $DIR
 cd $DIR
-git checkout -- CNAME
 git add -A
 git commit -m "update website"
 git push
